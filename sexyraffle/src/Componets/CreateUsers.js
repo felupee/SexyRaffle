4db247a3c2aa5497f19790d5dfcf3fca911881cf
felupee <felipesantosof@gmail.com>
2023-04-06 12:12:46 -0300

    adicionando desafios e melhorando o css

--- sexyraffle/src/Componets/CreateUsers.js
+++ sexyraffle/src/Componets/CreateUsers.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import ChallengeContext from '../Context/ChallengeContext';
 import { useNavigate } from 'react-router-dom';
+import './CreateUsers.css';
 
 function CreateUsers() {
   const { saveUsers } = useContext(ChallengeContext);
@@ -18,17 +19,18 @@ function CreateUsers() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Jogador 1:
-        <input type="text" value={jogador1} onChange={(e) => setJogador1(e.target.value)} />
-      </label>
-      <label>
-        Jogador 2:
-        <input type="text" value={jogador2} onChange={(e) => setJogador2(e.target.value)} />
-      </label>
-      <button type="submit">Salvar jogadores</button>
-    </form>
+<form class="form" onSubmit={handleSubmit}>
+  <label class="form__label">
+    <span class="form__label-text">Jogador 1</span>
+    <input type="text" value={jogador1} onChange={(e) => setJogador1(e.target.value)} class="form__input" />
+  </label>
+  <label class="form__label">
+    <span class="form__label-text">Jogador 2</span>
+    <input type="text" value={jogador2} onChange={(e) => setJogador2(e.target.value)} class="form__input" />
+  </label>
+  <button type="submit" class="form__button">Salvar jogadores</button>
+</form>
+
   );
 }
 
--- sexyraffle/src/Componets/Home.js
+++ sexyraffle/src/Componets/Home.js
@@ -4,11 +4,21 @@ import ChallengeContext from '../Context/ChallengeContext';
 
 function Home() {
   const stage1 = [
-    'Tome uma dose', 'Tome duas doses', 'Tire uma peça de roupa', 'Coloque uma peça de roupa',
-    'Seu parceiro tem que tomar uma dose', 'Beije seu parceiro'
+    'Tome um shot', 'Tome um shot', 'Tome um shot', 'Tome dois shots', 'Seu parceiro toma um shot',
+    'Faça uma massagem no seu parceiro', 'Dê um beijo no seu parceiro', 'Beije suavemente as costa do seu parceiro',
+    'Chupe o pescoço do seu parceiro', 'Dê um shot ao seu parceiro usando sua boca',
+
   ];
-  const stage2 = ['desafio1', 'desafio2', 'desafio3', 'desafio4', 'desafio5', 'desafio5'];
-  const stage3 = ['prenda1','prenda2', 'prenda3', 'prenda4', 'prenda5', 'prenda6'];
+  const stage2 = [
+    'Tome um shot', 'Tome um shot', 'Tome dois shots', 'Tire uma peça de roupa',
+    'Coloque uma peça de roupa', 'Chupe os seios do seu parceiro', 'Tire uma peça de roupa do seu parceiro',
+    'Masturbe seu parceiro por cima da roupa', 'Beije os arredores da região íntima do seu parceiro',
+    'Chupe a língua do seu parceiro'
+];
+  const stage3 = [
+    'Tome um shot', 'Tome um shot', 'Tire uma peça de roupa', 'Tire uma peça de roupa',
+    'Tire um peça de roupa do seu parceiro', 'Masturbe seu parceiro', 'Chupe seu parceiro',
+    'Beijo grego no seu parceiro', '69 com seu parceiro', 'Beijo com Halls'];
   const [challenge, setChallenge] = useState(stage1);
   const { users } = useContext(ChallengeContext)
 
@@ -30,11 +40,11 @@ function Home() {
   };
 
   const raffleNumber = () => {
-    const number = Math.floor(Math.random() * 6);
+    const number = Math.floor(Math.random() * 10);
     setRandomNumber(number)
     selectStage();
     setStage(prevState => prevState + 1)
-    console.log(users);
+    console.log(number);
   };
 
 
@@ -48,6 +58,7 @@ function Home() {
 
   return (
     <div className='div-main'>
+      <h1>Rodada: {round}</h1>
       <div className='div-jogador1'>
         <h1>{users[0]}</h1>
         <div className='div-doses'>
